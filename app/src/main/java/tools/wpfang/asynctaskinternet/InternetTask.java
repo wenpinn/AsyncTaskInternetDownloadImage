e372877f617dba8d9ab1bd5c1ac36414fabe2d28
Wen-Pinn Fang <Wen_Pinn@hotmail.com>
2018-07-12 14:19:36 +0800

    html parse json

--- app/src/main/java/tools/wpfang/asynctaskinternet/InternetTask.java
+++ app/src/main/java/tools/wpfang/asynctaskinternet/InternetTask.java
@@ -2,9 +2,12 @@ package tools.wpfang.asynctaskinternet;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.text.Html;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -17,16 +20,19 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class InternetTask extends AsyncTask<String,Void,String> {
-    private TextView resultText;
+public class InternetTask extends AsyncTask<String,Void,Bitmap> {
+    private ImageView resultText;
+    Bitmap bmp;
     private ProgressDialog pd;
     private Context ctx=null;
     String json_txt;
-    public InternetTask(Context ct,TextView tv)
+    public InternetTask(Context ct,ImageView tv)
     {
         ctx=ct;
         resultText=tv;
     }
+
+
 
     @Override
     protected void onPreExecute() {
@@ -39,7 +45,7 @@ public class InternetTask extends AsyncTask<String,Void,String> {
     }
 
     @Override
-    protected String doInBackground(String... strings) {
+    protected Bitmap doInBackground(String... strings) {
         String strUrl=strings[0];
         InputStream fin=null;
         String result_Txt="";
@@ -50,44 +56,20 @@ public class InternetTask extends AsyncTask<String,Void,String> {
             con.setConnectTimeout(20000);
             con.connect();
             fin=con.getInputStream();
-            BufferedReader bfRead=new BufferedReader((new InputStreamReader(fin)));
-            String line;
-            while((line=bfRead.readLine())!=null)
-            {
-             //   line=line.replaceAll("<","<b><");
-             //  line=line.replaceAll(">","></b>");
-                result_Txt+=line+"   \n";
-            }
-
-
-            JSONObject topObject=new JSONObject((result_Txt));
-            JSONObject secondObject=topObject.getJSONObject("result");
-            Log.i("testjson",topObject.toString());
-        //   JSONObject thirdObject=topObject.getJSONObject("result");
-            JSONArray infoArray=secondObject.getJSONArray("fields");
-
-            for(int i=0;i<infoArray.length();i++){
-                JSONObject obj=infoArray.getJSONObject(i);
-                String web=(String)obj.getString("id");
-                json_txt+=web+"\n";
-            }
-
-            result_Txt=json_txt+result_Txt;
-
-
-            bfRead.close();
+            bmp= BitmapFactory.decodeStream(fin);
+         //   result_Txt=json_txt+result_Txt;
             fin.close();
-            return result_Txt;
+            return bmp;
         } catch (Exception e) {
             e.printStackTrace();
-            return "No Data";
+            return null;
         }
     }
 
     @Override
-    protected void onPostExecute(String s) {
-        resultText.setText(Html.fromHtml(s));
-       // resultText.setText(s);
+    protected void onPostExecute(Bitmap s) {
+       // resultText.setText(Html.fromHtml(s));
+        resultText.setImageBitmap(s);
         pd.dismiss();
     }
 }
--- app/src/main/java/tools/wpfang/asynctaskinternet/MainActivity.java
+++ app/src/main/java/tools/wpfang/asynctaskinternet/MainActivity.java
@@ -3,21 +3,23 @@ package tools.wpfang.asynctaskinternet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView webResultText;
+    ImageView webResultText;
     InternetTask internetTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webResultText=findViewById(R.id.web_result);
+        webResultText=findViewById(R.id.imageView);
     }
 
     public void doWebConnect(View view) {
         internetTask=new InternetTask(this,webResultText);
        // internetTask.execute("http://140.138.146.85/publish.htm");
-        internetTask.execute(" https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=rdec-key-123-45678-011121314");
+       // internetTask.execute(" https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=rdec-key-123-45678-011121314");
+        internetTask.execute("http://wallscollection.net/wp-content/uploads/2016/12/Collection-of-Cool-Background-Images.jpg");
     }
 }
